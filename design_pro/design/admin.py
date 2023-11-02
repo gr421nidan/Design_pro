1de6eb8c44299fd0387599cadf3c1f8c169fe3d0
gr421nidan <nikushkina.dasha@yandex.ru>
2023-11-02 11:48:21 +0700

    Создание модели пользователя

--- design_pro/design/admin.py
+++ design_pro/design/admin.py
@@ -1,6 +1,3 @@
-# from django.contrib import admin
-#
-# from .models import *
-#
-#
-# admin.site.register(User)
+from django.contrib import admin
+from .models import *
+admin.site.register(CustomUser)
