50b3e6c09f36d818b5551dd49aa918c4571f3579
gr421nidan <nikushkina.dasha@yandex.ru>
2023-11-02 21:06:18 +0700

    Добавила шаблон для заявок и немного изменила основной шаблон

--- design_pro/design_pro/urls.py
+++ design_pro/design_pro/urls.py
@@ -1,10 +1,9 @@
 
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('superadmin/', admin.site.urls),
